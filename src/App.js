c3644644dd302eb9527436289655a34ca434800e
Bivek Gurung <bivek.tamu@gmail.com>
2020-09-09 11:57:43 +1000

    Integrated backend and frontend

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Video from './Video';
+import axios from './axios';
+
 
 function App() {
+
+  const [videos, setVideos] = useState([]);
+
+  useEffect(() => {
+    async function fetchPosts() {
+      const response = await axios.get('/posts');
+      setVideos(response.data);
+
+      return response;
+
+    }
+
+    fetchPosts();
+
+  }, [])
+
   return (
     // BEM naiming convention
     <div className="app">
@@ -11,9 +29,20 @@ function App() {
       {/* app container */}
       <div className="app_videos">
 
-        {/* videos */}
-        <Video />
-        <Video />
+        {
+          videos.map(({ _id, url, channel, description, song, likes, shares, comments }) => (
+            <Video key={_id}
+              channel={channel}
+              description={description}
+              song={song}
+              likes={likes}
+              shares={shares}
+              comments={comments}
+              url={url}
+            />
+          ))
+        }
+
       </div>
 
 
--- src/Video.js
+++ src/Video.js
@@ -1,14 +1,17 @@
-import React, { useRef, useState } from 'react'
+import React, { useRef, useState, useEffect } from 'react'
 
 import './Video.css';
 import VideoFooter from './VideoFooter';
 import VideoSidebar from './VideoSidebar';
 
 
-function Video() {
+function Video({ url, channel, description, song, likes, shares, comments }) {
+
+
 
     const [playing, setPlaying] = useState(false);
-    const videoRef = useRef(null)
+    const videoRef = useRef(null);
+
 
     const onClick = () => {
         // if video is playing 
@@ -24,6 +27,8 @@ function Video() {
         // otherwise play it...
 
     }
+
+
     return (
         <div className="video">
             <video
@@ -31,13 +36,16 @@ function Video() {
                 ref={videoRef}
                 onClick={onClick}
                 loop
-                src="https://v16m.tiktokcdn.com/b9797612988a1508e4e7ef42cbfd880e/5f574b02/video/tos/useast2a/tos-useast2a-ve-0068c002/7478d57011cd43f19f7fa282e5fc875f/?a=1233&br=1488&bt=744&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20200908031209010189072034312F2A7B&lr=tiktok_m&mime_type=video_mp4&qs=0&rc=ajRsNXJyZmpmdzMzNzczM0ApOzhlaTg2Nzs8N2k8N2g3ZmdebmswbnFtajVfLS0zMTZzczMxM2FjNF9fYDE1MC0xMi06Yw%3D%3D&vl=&vr="></video>
+                src={url}></video>
 
             {/* videofooter */}
-            <VideoFooter />
+            <VideoFooter
+                channel={channel}
+                description={description}
+                song={song} />
 
             {/* videosidefar */}
-            <VideoSidebar likes={100} shares={50} comments={200} />
+            <VideoSidebar likes={likes} shares={shares} comments={comments} />
         </div>
     )
 }
